5b252d50b573010ec4545f39a88b277b9c543369
jamestariga <jamestariga08@live.com>
2022-01-02 15:54:49 -0800

    Final

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 const num = (val) => {
     document.getElementById('calc').value += val;
 }
+
+const equal = () => {
+    let input = document.getElementById('calc').value;
+    let output = eval(input);
+    document.getElementById('calc').value = output;
+}
+
+const clr = () => {
+    document.getElementById('calc').value = '';
+}
+
+const del = () => {
+    let bksp = document.getElementById('calc').value;
+    document.getElementById("calc").value = bksp.substring(0,bksp.length-1);
+}
